161f362987939926f2380cdde1b0494b2556a4b8
Azat Akhunov <azat.akhunov@protonmail.com>
2021-03-21 21:18:22 +0300

    three questions added

--- src/main/java/interview/four/Question.java
+++ src/main/java/interview/four/Question.java
@@ -8,15 +8,15 @@ public class Question {
     public static void main(String[] args) {
         String s1 = "java-best";
         final String s2 = "java";
-        String s3 = "11";
+        String s3 = "-best";
         String s4 = "java-best";
 
-        System.out.println(s1 == s4);         // #1
-        System.out.println(s1 == s2 + s3);    // #2
+        System.out.println(s1 == s4);          // #1
+        System.out.println(s1 == s2 + s3);     // #2
         System.out.println(s1 == s2 + "-best");// #3
 
         /*
-
+        Правильный ответ  #1 и #3 так как s2 тут является final и java при конкатенации  создаст новую переменную с новой ссылкой
          */
     }
 }
