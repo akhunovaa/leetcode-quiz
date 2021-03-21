67b9911a2943eb5c75245bd4edf29f530c1cdd7d
Azat Akhunov <azat.akhunov@protonmail.com>
2021-03-21 22:35:46 +0300

    three questions added

---
+++ src/main/java/interview/eight/Question.java
@@ -0,0 +1,36 @@
+package interview.eight;
+
+public class Question {
+
+    /**
+     * Какой будет вывод?
+     * 1# Thread end. и зависнет
+     * 2# Thread end.Main end
+     * 3# Результат неизвестен
+     */
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> System.out.print("Thread end."));
+
+        synchronized (thread) {
+            thread.start();
+            thread.wait();
+        }
+
+        System.out.print("Main end");
+    }
+
+    /*
+    Правильный ответ #2
+    Thread end.Main end
+
+    Потому что у Thread автоматически вызовется notifyAll();
+     */
+
+    /*
+    Synchronization on local variable 'thread'
+ Inspection info: Reports synchronization on a local variable or parameter.
+ It is very difficult to guarantee correctness when such synchronization is used.
+ It may be possible to improve code like this by controlling access through e.g.
+ a synchronized wrapper class, or by synchronizing on a field.
+     */
+}
