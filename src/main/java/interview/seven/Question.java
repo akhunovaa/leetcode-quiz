f011a1fbbe3d9ac99a5d3e02f1e79ce2bec089e7
Azat Akhunov <azat.akhunov@protonmail.com>
2021-03-21 22:34:55 +0300

    three questions added

--- src/main/java/interview/seven/Question.java
+++ src/main/java/interview/seven/Question.java
@@ -3,21 +3,34 @@ package interview.seven;
 public class Question {
 
     /**
-     * Какой будет результат?
-     * 1# ArrayIndexOutOfBoundsException
-     * 2# ArithmeticException
-     * 3# Завершится без ошибок
-     * 4# Ошибка компиляции
+     * Какой будет вывод?
+     * 1# Thread end. и зависнет
+     * 2# Thread end.Main end
+     * 3# Результат неизвестен
      */
-    public static void main(String[] args) {
-       int[] arr = new int[1];
-       int i = 2;
-       arr[--i] = 1 / --i;
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> System.out.print("Thread end."));
+
+        synchronized (thread) {
+            thread.start();
+            thread.wait();
+        }
+
+        System.out.print("Main end");
     }
 
     /*
-    Правильный ответ #2 Потому что на ноль делить нельзя
-    Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 1 out of bounds for length 1
-	at interview.seven.Question.main(Question.java:15)
+    Правильный ответ #2
+    Thread end.Main end
+
+    Потому что у Thread автоматически вызовется notifyAll();
+     */
+
+    /*
+    Synchronization on local variable 'thread'
+ Inspection info: Reports synchronization on a local variable or parameter.
+ It is very difficult to guarantee correctness when such synchronization is used.
+ It may be possible to improve code like this by controlling access through e.g.
+ a synchronized wrapper class, or by synchronizing on a field.
      */
 }
