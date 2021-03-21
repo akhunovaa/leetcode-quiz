0151aed619aefc733db694b92a3fa464a29b594e
Azat Akhunov <azat.akhunov@protonmail.com>
2021-03-21 23:08:25 +0300

    three questions added

--- src/main/java/interview/nine/Question.java
+++ src/main/java/interview/nine/Question.java
@@ -1,39 +1,33 @@
 package interview.nine;
 
-import java.util.ArrayList;
-import java.util.List;
 
-@SuppressWarnings({"rawtypes", "unchecked"})
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Question {
 
     /**
      * Какой будет вывод?
-     * #1 3
-     * #2 4
-     * #3 Ошибка компиляции
-     * #4 Runtime exception
+     * #1 1
+     * #2 2
+     * #3 3
+     * #4 ClassCastException
+     * #5 Ошибка компиляции
      */
     public static void main(String[] args){
-        List longs = new ArrayList<Long>();
-        longs.add(1L);
-        longs.add(1.0);
-        longs.add(new Object());
-        longs.add("I'm a LONG!!! Yeeep that is true!");
+        Set<Number> set = new TreeSet<Number>();
+        set.add(1);
+        set.add(1L);
+        set.add(1.0);
 
-        System.out.println(longs.size());
+        System.out.println(set.size());
     }
 
     /*
-    Правильный ответ #2
-    Так как на этапе кмпиляции без разницы что складывать. Но в Runtime такое не прйдет.
-    А вот такое не скомпилируется, ссылаясь на то что нельзя складывать разные типы ланных:
-        List<Long> longs = new ArrayList();
-        longs.add(1L);
-        longs.add(1.0);
-        longs.add(new Object());
-        longs.add("I'm a LONG!!! Yeeep that is true!");
-
-        System.out.println(longs.size());
+    Правильный ответ #4 ClassCastException
+    Для TreeSet сработает компаратор и при java.lang.Long.compareTo(Long.java:65) выбросится ClassCastException
+    class java.lang.Integer cannot be cast to class java.lang.Long
+    Потому что это дерево и ему необходимо отсортироваться
      */
 
 }
