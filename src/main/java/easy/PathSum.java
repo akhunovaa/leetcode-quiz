d33267280544ae82b3c77112783b59fb179b78d8
Azat Akhunov <azat.akhunov@protonmail.com>
2022-10-10 17:17:13 +0300

    new updates

---
+++ src/main/java/easy/PathSum.java
@@ -0,0 +1,50 @@
+package easy;
+
+/*
+    Input: root = [1,2,3], targetSum = 5
+    Output: false
+    Explanation: There two root-to-leaf paths in the tree:
+    (1 --> 2): The sum is 3.
+    (1 --> 3): The sum is 4.
+    There is no root-to-leaf path with sum = 5.
+ */
+/*
+    5,4,8,11,null,13,4,7,2,null,null,null,1
+ */
+public class PathSum {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null) {
+            return sum == root.val;
+        }
+
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
