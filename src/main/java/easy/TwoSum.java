0f47c60906a1e86e4bc59e52daee44463665ddc8
Azat Akhunov <azat.akhunov@protonmail.com>
2022-10-26 15:31:38 +0300

    new updates

--- src/main/java/easy/TwoSum.java
+++ src/main/java/easy/TwoSum.java
@@ -15,7 +15,7 @@ public class TwoSum {
     Time complexity : O(n^2)
     Space complexity : O(1)
      */
-    public static int[] twoSum(int[] nums, int target) { // nums = {2, 11, 7, 15} target = 9
+    public static int[] twoSum2(int[] nums, int target) { // nums = {2, 11, 7, 15} target = 9
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] == target - nums[i]) { // 11 ==  9 - 2 =>  7 ==  9 - 2
@@ -30,10 +30,10 @@ public class TwoSum {
     Time complexity : O(n)
     Space complexity : O(n)
     */
-    public static int[] twoSum2(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) { // nums = {2, 11, 7, 15} target = 9
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
+            map.put(nums[i], i); // {2=0, 11=1, 7=2, 15=3} // добавление в HashMap не упорядочен
         }
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i]; // 9 - 2 = 7
