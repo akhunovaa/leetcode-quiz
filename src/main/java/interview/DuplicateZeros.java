a164f6bb0021abc17edee611474d3a1caa48d852
Azat Akhunov <azat.akhunov@protonmail.com>
2022-11-06 22:41:30 +0300

    new updates

---
+++ src/main/java/interview/DuplicateZeros.java
@@ -0,0 +1,42 @@
+package interview;
+
+import java.util.Arrays;
+
+public class DuplicateZeros {
+
+    public static void main(String[] args) {
+        int[] input = {-4, -1, 0, 3, 10};
+        duplicateZeros(input);
+
+    }
+
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public static void duplicateZeros(int[] arr) {
+        int possibleDups = 0;
+        int length_ = arr.length - 1;
+
+        for (int left = 0; left <= length_ - possibleDups; left++) {
+            if (arr[left] == 0) {
+                if (left == length_ - possibleDups) {
+                    arr[length_] = 0;
+                    length_ -= 1;
+                    break;
+                }
+                possibleDups++;
+            }
+        }
+
+        int last = length_ - possibleDups;
+
+        for (int i = last; i >= 0; i--) {
+            if (arr[i] == 0) {
+                arr[i + possibleDups] = 0;
+                possibleDups--;
+                arr[i + possibleDups] = 0;
+            } else {
+                arr[i + possibleDups] = arr[i];
+            }
+        }
+    }
+}
