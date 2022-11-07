73b55bf4d7115c79a19feefbd59dbaed98448157
Azat Akhunov <azat.akhunov@protonmail.com>
2022-11-07 19:19:02 +0300

    new updates Move Zeroes

---
+++ src/main/java/interview/array/MoveArrayZeroes.java
@@ -0,0 +1,41 @@
+package interview.array;
+
+import java.util.Arrays;
+
+/**
+ * Move Zeroes
+ * <p/>
+ * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ * Note that you must do this in-place without making a copy of the array.
+ * <p/>
+ * Example 1:
+ * Input: nums = [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ * <p/>
+ * Example 2:
+ * Input: nums = [0]
+ * Output: [0]
+ */
+public class MoveArrayZeroes {
+
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+        System.out.println("Answer: " + Arrays.toString(nums));
+    }
+
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public static void moveZeroes(int[] nums) {
+        int lastNonZeroFoundAt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZeroFoundAt++] = nums[i];
+            }
+        }
+        for (int i = lastNonZeroFoundAt; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
