ade8e6ea3145e4b7af1bb85a02a53901b1fa33e0
Azat Akhunov <azat.akhunov@protonmail.com>
2022-11-17 17:14:09 +0300

    new update -  Reverse Linked List

--- src/main/java/interview/collection/RotateList.java
+++ src/main/java/interview/collection/RotateList.java
@@ -22,12 +22,32 @@ public class RotateList {
                                 new ListNode(4,
                                         new ListNode(5)))));
         int k = 2;
-        ListNode answer = rotateRight(head, k);
+        // ListNode answer = rotateRight(head, k);
+        ListNode answer = reverseList(head);
         while (answer != null) {
             System.out.println("Answer: " + answer);
             answer = answer.next;
         }
 
+    }
+
+    static ListNode reverseList(ListNode head) {
+        // граничный случай
+        // больше необходим для рекрсивного вызова для понимания границы последнего элемента
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // при помощи рекурсии доходим до последнего элемента в списке и устанавливаем его на переменную res => [value: 5]
+        ListNode res = reverseList(head.next);
+        // head при рекурсивном последнем вызове будет иметь значение head = [value: 4]
+        // значение у head.next будет висеть head.next = [value: 4]
+        // где head.next.next понятное дело будет null (так как у head.next со значением [value: 5] является последним элементом в списке
+        // его то и нужно установить на head со значением [value: 4] чтобы от [value: 5] => next был [value: 4]
+        head.next.next = head;
+        // и head.next устанавливаем в null чтобы у [value: 4] next значения не было: [value: 5] => [value: 4] => null
+        head.next = null;
+        // возвращаем предыдущему рекурсивному вызову сформированный список res = [value: 5] => [value: 4]
+        return res;
     }
 
     public static ListNode rotateRight(ListNode head, int k) {
