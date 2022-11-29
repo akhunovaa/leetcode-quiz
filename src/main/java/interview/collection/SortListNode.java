f29baf51825907d29764f9bdf3e4dbeabf23e6a1
Azat Akhunov <azat.akhunov@protonmail.com>
2022-11-29 17:23:47 +0300

    new update - SortListNode

---
+++ src/main/java/interview/collection/SortListNode.java
@@ -0,0 +1,67 @@
+package interview.collection;
+
+public class SortListNode {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(6,
+                new ListNode(5,
+                        new ListNode(4,
+                                new ListNode(3,
+                                        new ListNode(2,
+                                                new ListNode(1))))));
+
+        ListNode answer = sortList(head);
+        while (answer != null) {
+            System.out.println("Answer: " + answer);
+            answer = answer.next;
+        }
+
+    }
+
+    public static ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+
+
+    static ListNode getMid(ListNode head) {
+        ListNode midPrev = null;
+        while (head != null && head.next != null) {
+            if (midPrev == null) {
+                midPrev = head;
+            } else {
+                midPrev = midPrev.next;
+            }
+            head = head.next.next;
+        }
+        ListNode mid = midPrev.next;
+        midPrev.next = null;
+        return mid;
+    }
+
+    static ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+                tail = tail.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+                tail = tail.next;
+            }
+        }
+        if (list1 != null) {
+            tail.next = list1;
+        }else {
+            tail.next = list2;
+        }
+        return dummyHead.next;
+    }
+}
