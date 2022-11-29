4684e893720f9fd7d4d7f8f5e3a9e27ae9bcbeef
Azat Akhunov <azat.akhunov@protonmail.com>
2022-11-29 16:28:41 +0300

    new update - Longest Palindrome

--- src/main/java/interview/string/LongestPalindrome.java
+++ src/main/java/interview/string/LongestPalindrome.java
@@ -1,6 +1,8 @@
 package interview.string;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -26,6 +28,9 @@ public class LongestPalindrome {
         String s = "abccccdd"; // dccaccd
         int answer = longestPalindrome(s);
         System.out.println("Answer: " + answer);
+
+        answer = longestPalindrome(new String[] {"ab","ty","yt","lc","cl","ab"}); // ab cl lc yt ty ab // ab ab yt ty cl lc
+        System.out.println("Answer: " + answer);
     }
 
     public static int longestPalindrome(String s) {
@@ -40,6 +45,43 @@ public class LongestPalindrome {
                 ans++;
         }
         return ans;
+    }
+
+    public static int longestPalindrome(String[] words) {
+        HashMap<String, Integer> count = new HashMap<>();
+        for (String word : words) {
+            Integer countOfTheWord = count.get(word);
+            if (countOfTheWord == null) {
+                count.put(word, 1);
+            } else {
+                count.put(word, countOfTheWord + 1);
+            }
+        }
+        int answer = 0;
+        boolean central = false;
+        for (Map.Entry<String, Integer> entry : count.entrySet()) {
+            String word = entry.getKey();
+            int countOfTheWord = entry.getValue();
+            // if the word is a palindrome
+            if (word.charAt(0) == word.charAt(1)) {
+                if (countOfTheWord % 2 == 0) {
+                    answer += countOfTheWord;
+                } else {
+                    answer += countOfTheWord - 1;
+                    central = true;
+                }
+                // consider a pair of non-palindrome words such that one is the reverse of another
+            } else if (word.charAt(0) < word.charAt(1)) {
+                String reversedWord = "" + word.charAt(1) + word.charAt(0);
+                if (count.containsKey(reversedWord)) {
+                    answer += 2 * Math.min(countOfTheWord, count.get(reversedWord));
+                }
+            }
+        }
+        if (central) {
+            answer++;
+        }
+        return 2 * answer;
     }
 
     // данный способ перебирает все символы из строки и при следующем нахождении идентичного символа, удаляет из Set
