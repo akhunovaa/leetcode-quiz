056b1d98dc1f35f8070315612e86e7d9d7b7c9a1
Azat Akhunov <azat.akhunov@protonmail.com>
2022-11-30 14:05:14 +0300

    new update - ☕ JAVA | JewelsStones ☕

---
+++ src/main/java/interview/string/JewelsStones.java
@@ -0,0 +1,21 @@
+package interview.string;
+
+public class JewelsStones {
+
+    public static void main(String[] args) {
+        String jewels = "aA", stones = "aAAbbbb";
+        int answer = numJewelsInStones(jewels, stones);
+        System.out.println("Answer: " + answer);
+    }
+
+
+    static int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for (char c : stones.toCharArray()) {
+            if (jewels.indexOf(c) != -1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
