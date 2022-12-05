740baab94a67a6d6a208e6f06a37c755928836a6
Azat Akhunov <azat.akhunov@protonmail.com>
2022-12-05 18:43:58 +0300

    new updates ☕ Clear Java Code | Replace Digits With Characters ☕

---
+++ src/main/java/interview/string/ReplaceDigitsWithCharacters.java
@@ -0,0 +1,28 @@
+package interview.string;
+
+public class ReplaceDigitsWithCharacters {
+
+    public static void main(String[] args) {
+        String s = "a1b2c3d4e";
+        String answer = replaceDigits(s);
+        System.out.println("Answer: " + answer);
+    }
+
+    static String replaceDigits(String s) {
+        char[] result = new char[s.length()];
+
+        result[0] = s.charAt(0);
+        for(int i = 1; i < s.length(); i++){
+            if(Character.isLetter(s.charAt(i))) {
+                result[i] = s.charAt(i);
+                i++;
+                if(i == s.length()) break;
+            }
+            int shift = Character.getNumericValue(s.charAt(i * 2 - 1));
+            char replace = (char) (s.charAt(i - 1) + shift);
+            result[i] = replace;
+        }
+
+        return String.valueOf(result);
+    }
+}
