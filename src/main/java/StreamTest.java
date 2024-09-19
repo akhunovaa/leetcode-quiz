0fde72c51b94701a6051ffb4f9cc6c398e66e53a
Azat Akhunov <azat.akhunov@protonmail.com>
2024-09-19 20:09:06 +0300

    new updates ☕ Clear Java Code ☕

--- src/main/java/StreamTest.java
+++ src/main/java/StreamTest.java
@@ -25,6 +25,11 @@ public class StreamTest {
             add(2);
             add(null);
             add(5);
+            add(6);
+            add(7);
+            add(8);
+            add(4);
+            add(null);
         }};
 
 
